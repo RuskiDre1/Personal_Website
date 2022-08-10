b80d1aeba8ca300a87bd6c6fe65d7a55a2ad9404
Ruskidre <andrey8@pdx.edu>
2022-08-10 15:18:53 -0700

    Added library for symbols for Linked in, git , instagram,

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ window.onload = function ondocload() {
               window.onscroll = function() {scrollFunction()};
               
               function scrollFunction() {
-                if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
                   mybutton.style.display = "block";
                 } else {
                   mybutton.style.display = "none";
                 }
               }
 }
+// arrow button scroll to top
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-
-}
+  }
